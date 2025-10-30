a8ee5e2313ba6c66c2786abf57ac75ba757f686f
Shukurov_Khan <kamoliddinkhan907@gmail.com>
2025-10-30 15:02:18 +0500

    updated CustomForm, ToDoList

--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,15 +1,30 @@
-const CustomForm = () => {
+import { useState } from "react";
+
+const CustomForm = ({ onAdd }) => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim() === "") return;
+    onAdd(task);
+    setTask("");
+  };
+
   return (
-    <form className="flex items-center gap-3 bg-white p-4 rounded-2xl shadow-md">
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center gap-3 bg-white p-4 rounded-2xl shadow-md"
+    >
       <input
         type="text"
-        placeholder="Add new task"
-        className="flex-1 border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add new task..."
+        className="flex-1 border border-gray-300 rounded-xl px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
       />
-
       <button
         type="submit"
-        className="border border-red-500 px-2 py-1.5 rounded-md cursor-pointer bg-red-500 text-white hover:bg-red-700"
+        className="bg-blue-600 text-white px-5 py-2 rounded-xl hover:bg-blue-700 transition-colors"
       >
         Add
       </button>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,45 @@
+import { useState } from "react";
 import CustomForm from "./CustomForm";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTask = (task) => {
+    setTodos((prev) => [...prev, task]);
+  };
+
+  const removeTask = (index) => {
+    setTodos((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  console.log("TODOS_TEST:", todos);
+
   return (
     <div className="max-w-md mx-auto space-y-6">
       <h2 className="text-2xl font-bold text-center">🧑🏻‍💻 To-Do List</h2>
 
-      <CustomForm />
+      <CustomForm onAdd={addTask} />
+
+      <ul className="bg-white rounded-2xl shadow-md divide-y divide-gray-200">
+        {todos.length === 0 ? (
+          <li className="text-center text-gray-500 py-4">No task yet</li>
+        ) : (
+          todos.map((todo, index) => (
+            <li
+              key={index}
+              className="flex items-center justify-between px-4 py-3 font-medium"
+            >
+              <span className="text-xl">{todo}</span>
+              <button
+                onClick={() => removeTask(index)}
+                className="cursor-pointer text-[8px] px-1.5 py-1.5 rounded-2xl bg-gray-200 hover:bg-gray-300 transition-colors"
+              >
+                ❌
+              </button>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 };
